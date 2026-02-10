a3f58e5b91b9186389f8c3104e08f6ad991e5fd9
arya-abadi <arya.abadi89@gmail.com>
2026-02-10 15:05:53 +0100

    add: memoize

--- src/components/Products/ProductList/ProductList.tsx
+++ src/components/Products/ProductList/ProductList.tsx
@@ -1,8 +1,19 @@
 import { ProductListProps } from '../../../interfaces/IProduct.ts';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem.tsx";
+import { memoize } from "../../../utils/memoize";
 
 const ProductList= ({ products } : ProductListProps) => {
+
+    const slowFn = (a, b) => {
+        console.log("calculating ...");
+        return a + b;
+    }
+    const fastFn = memoize(slowFn);
+    fastFn(6,5);
+    fastFn(2,5);
+    fastFn(2,5);
+
     return (
         <div className="product-grid">
             {products.map((product) => (
